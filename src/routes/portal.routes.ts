e5171591badd9574de914080fe40daf43e5c8659
Divyansh Pal <essencedivine.connect@gmail.com>
2026-09-07 10:55:24 +0530

    Pet

--- src/routes/portal.routes.ts
+++ src/routes/portal.routes.ts
@@ -58,7 +58,10 @@ router.post('/screening/reports/:id/upload', uploadScreeningReportDoc.single('do
 
 router.get('/violations', (req, res, next) => portalController.getViolations(req, res, next));
 router.post('/violations', (req, res, next) => portalController.createViolation(req, res, next));
+router.get('/violations/sync-dob', (req, res, next) => portalController.syncNycDobViolations(req, res, next));
 router.post('/violations/sync-dob', (req, res, next) => portalController.syncNycDobViolations(req, res, next));
+router.get('/violations/sync-nyc-dob', (req, res, next) => portalController.syncNycDobViolations(req, res, next));
+router.post('/violations/sync-nyc-dob', (req, res, next) => portalController.syncNycDobViolations(req, res, next));
 
 // General Dashboard User Profile Views
 router.get('/user/profile', (req, res, next) => portalController.getUserProfile(req, res, next));
